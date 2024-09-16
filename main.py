7048c1aca2a701615d3d13b10bac9591d796bed2
fonwenu23 <fonwenu@gmail.com>
2024-09-16 07:26:11 -0400

    updating keys

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 # load_dotenv(dotenv_path)
 
 # Currently using default location.
-load_dotenv()
+load_dotenv(override=True)
 
 client_id = os.getenv('client_id')
 client_secret = os.getenv('client_secret')
@@ -18,10 +18,17 @@ client_secret = os.getenv('client_secret')
 auth_manager = SpotifyClientCredentials(client_id=client_id,client_secret=client_secret)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-drake_uri = 'spotify:artist:3TVXtAsR1Inumwj472S9r4'
-results = sp.artist_top_tracks(drake_uri)
-for track in results['tracks'][:10]:
-    print('track    : ' + track['name'])
-    # print('audio    : ' + track['preview_url'])
-    print('cover art: ' + track['album']['images'][0]['url'])
-    print()
+# Search for Drake by artist name
+results = sp.search(q='Drake', type='artist')
+
+# Get Drake's artist ID
+artist_id = results['artists']['items'][0]['id']
+
+tracks = sp.artist_top_tracks(artist_id)
+# Print the names of Artist's top tracks
+
+# print(tracks)
+
+for track in tracks['tracks'][:10]:
+    print(track['name'])
+   
